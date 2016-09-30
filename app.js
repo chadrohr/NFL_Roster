5e4dde18b4aae42c23eab485426614cf21c1b793
Chad Rohr <chadrohr@yahoo.com>
2016-09-29 22:41:41 -0600

    added Array but not seperated by player

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var myPlayer = $('.player-roster')
-var myRoster
+var myRoster = [];
 function addToRoster(event){
-    event.preventDefault()
-  var name= $('#name').val()
+    event.preventDefault();
+  var name = $('#name').val()
   var position = $('#position').val()
   var jersey = $('#jersey').val()
-
+  myRoster.push(name,position,jersey);
+// for (var i = in myRoster){}
+// myRoster.push(myPlayer.name, myPlayer.position, myPlayer.jersey);
+//   myPlayer = {
+//       this.name = name,
+//       this.position = position,
+//       this.jersey = jersey,
+    
   myPlayer.append(`<div class="player-card">
                 <div>
                     <img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/"></div>
@@ -19,5 +26,10 @@ function addToRoster(event){
                     <p>${jersey}</p>
                 </div>
             </div>`)
-}
 
+
+// function buildMyRoster(name, position,jersey){
+//     for(i = 0; i < myRoster.length; i ++){
+        
+//     }
+}
